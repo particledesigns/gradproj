0870c0b2b7d4b51e87e593077f2d2385311a362d
Johannes Skjærbæk <john@particledesigns.dk>
2016-05-31 13:34:12 +0200

    js and nav updates

--- js/rest.js
+++ js/rest.js
@@ -1,6 +1,11 @@
 if ($(window).width() > 1024) {
-    alert('Your browser size is more than 1024px. This prototype is best viewed at the resolution of a typical iPad 2 - 1024px width. If you do not view it at this resolution, you are getting a sob-optimal experience.');
+    alert('Your browser size is more than 1024px. This prototype is best viewed in LANDSCAPE MODE at the resolution of a typical iPad 2 - 1024px width. If you do not view it at this resolution, you are getting a sob-optimal experience.');
 }
 else if ($(window).width() < 980){
-    alert('Your browser size is less than 960px. This prototype is best viewed at the resolution of a typical iPad 2 - 1024px width. If you do not view it at this resolution, you are getting a sob-optimal experience.');
+    alert('Your browser size is less than 960px. This prototype is best viewed in LANDSCAPE MODE at the resolution of a typical iPad 2 - 1024px width. If you do not view it at this resolution, you are getting a sob-optimal experience.');
 }
+
+function goBack() {
+    window.history.back();
+}
+
